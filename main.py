66576bb20f37fa144f6c79f154190c876c800004
Mark <aebolm@gmail.com>
2025-04-28 01:07:33 +0300

    Initial commit with new project

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
-import os
+# Heads and Tails Game
+
+import random
+
+def head_coin():
+    print("""
+         ~~~~~~~~
+       /\\         /\\
+      /  \\       /  \\
+     /    \\     /    \\
+    |      |   |      |
+    |  HEADS   |
+     \\         /
+      \\       /
+       \\~~~~~/
+     """)
+def tail_coin():
+    print("""
+         ~~~~~~~~
+       /\\         /\\
+      /  \\       /  \\
+     /    \\     /    \\
+    |      |   |      |
+    |  TAILS   |
+     \\         /
+      \\       /
+       \\~~~~~/
+     """)
 
 
-print("WELCOME TO FILTER YOUR PC PROGRAM".upper())
-print("Choose what filtration you want in your disk C:\n")
+def main_text():
+    print("\t\tWELCOME TO THE HEADS OR TAILS GAME!")
 
-print("1. Filter by file extension (e.g. .txt, .jpg, .pdf)")
-print("2. Filter by file size (e.g. files greater than 1MB)")
-print("3. Filter by folder or file name")
-print("4. Show all files and folders")
-print("5. Exit")
+    print("1) PLAY!")
+    print("2) INFO!")
+    print("3) EXIT!")
 
-try:
-    User_choice = int(input("\nEnter your variant: "))
-except ValueError:
-    print("Please enter number!")
-except Exception as e:
-    print(f"Error! {e}")
+    user_choose = input("Please select an option (1/2/3): ")
 
+def play_game(user_choose):
+    pass
 
+def info_game():
+    pass
 
-def choice_one():
-    extension = input("Enter extension (e.g. .txt): ")
-    print(f"Filtering by {extension}...")
-
-    for root,dirs,files in os.walk("C:\\"):
-        for file in files:
-            if file.endswith(extension):
-                print(os.path.join(root,file))
-def choice_two():
-    size = int(input("Min size in bytes (e.g. 1000000 for 1MB): "))
-    print(f"Filtering files > {size} bytes...")
-
-def choice_three():
-    name = input("Enter a name of the file | folder: ")
-    print(f"Filtering by name containing '{name}'...")
-
-def choice_four():
-    print("Showing all files in C:\\ ...")
-
-def choice_five():
-    print("Exiting...")
-
-
-
-
-
-
-
-
-
+def exit_game():
+    pass
